f461ed39fc9c9892ae8c1740020b266670d9d8ff
muharik19 <ahmadmuharik@gmail.com>
2018-02-16 23:07:20 +0700

    update admin lte

--- web/index.php
+++ web/index.php
@@ -1,6 +1,9 @@
 <?php
 
 // comment out the following two lines when deployed to production
+// defined('YII_DEBUG') or define('YII_DEBUG', false);
+// defined('YII_ENV') or define('YII_ENV', 'production');
+
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
