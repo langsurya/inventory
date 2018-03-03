e84ce2f76ea52170fcf6759bd88f0486cd3d6950
Elang Surya <elangsurya19@gmail.com>
2018-03-03 15:09:27 +0700

    setup modules inventory

---
+++ modules/master/controllers/BankController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\modules\master\controllers;
+
+use Yii;
+
+use yii\web\controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * BankController inplements the CRUD actions for Bank model.
+ */
+class BankController extends Controller
+{
+	/**
+     * @inheritdoc
+     */
+	public function behaviors()
+	{
+		return [
+			'verbs' => [
+				'class' => VerbFilter::className(),
+				'actions' => [
+					'delete' => ['POST'],
+				],
+			],
+		];
+	}
+
+	/**
+     * Lists all Bank models.
+     * @return mixed
+     */
+	public function actionIndex()
+	{
+		// $searchModel = new BankSearch();
+
+		return $this->render('index', [
+			
+		]);
+
+	}
+}
---
+++ modules/profile/Profile.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\modules\profile;
+
+/**
+ * pegawai module definition class
+ */
+class Profile extends \yii\base\Module
+{
+	/**
+     * @inheritdoc
+     */
+	public $controllerNamespace = 'app\modules\profile\controllers';
+	/**
+     * @inheritdoc
+     */
+	public function init()
+    {
+        parent::init();
+    }
+}
