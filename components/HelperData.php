994185256bccc61c2dedb7553495e8b3e2748300
Elang Surya <elangsurya19@gmail.com>
2018-03-08 23:02:33 +0700

    function agama

--- components/HelperData.php
+++ components/HelperData.php
@@ -8,6 +8,7 @@ use yii\helpers\ArrayHelper;
 
 use mdm\admin\models\User;
 use app\modules\master\models\Bank;
+use app\modules\profile\models\Pegawai;
 
 class HelperData extends \yii\base\Component
 {
@@ -20,11 +21,34 @@ class HelperData extends \yii\base\Component
 		return $data;	
 	}
 
+	public function agamaName($id)
+    {
+		$data = [1=>'Islam', 'Kristen Protestan', 'Kristen Katolik', 'Budha', 'Dll'];
+		foreach ($data as $key => $value) {
+			if ($key == $id) {
+				$data = $value;
+			}
+		}
+        return $data;
+    }
+
 	/* Nama bang */
     public function listBank()
     {
         $data = Bank::find()->all();
         return ArrayHelper::map($data, 'id_bank', 'nama_bank');
+    }
+
+    public function pegawaiName($id)
+    {
+        $pegawai = Pegawai::find()->select(['nama_pegawai'])->where(['user_id'=>$id])->one();
+        if(!empty($pegawai)){
+            return $pegawai->nama_pegawai;
+        } else {
+            return \Yii::$app->user->identity->username;
+        }
+
+        return 'No Name';
     }
 
 }
