5c68aafab0926b8035df1e57172d7173f4714972
muharik19 <ahmadmuharik@gmail.com>
2018-03-14 23:27:03 +0700

    fix categories

--- modules/master/views/categories/update.php
+++ modules/master/views/categories/update.php
@@ -5,9 +5,9 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\modules\master\models\Categories */
 
-$this->title = 'Update Categories: {nameAttribute}';
+$this->title = $model->name;
 $this->params['breadcrumbs'][] = ['label' => 'Categories', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id_category]];
+$this->params['breadcrumbs'][] = ['label' => $model->id_category, 'url' => ['view', 'id' => $model->id_category]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="categories-update">
